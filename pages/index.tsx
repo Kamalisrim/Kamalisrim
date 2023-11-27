1e0a8ccf8e42073e05d67cfbd72bf6ab790a9e6e
Kamalisrim <141914316+Kamalisrim@users.noreply.github.com>
2023-11-27 19:27:37 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+// pages/index.tsx
+import React, { useState } from 'react';
+import TextBox from '../components/TextBox';
+import Buttons from '../components/Buttons';
+
+const Home: React.FC = () => {
+  const [text, setText] = useState<string>('');
+
+  const handleLog = () => {
+    console.log('Text:', text);
+  };
+
+  const handleClear = () => {
+    setText('');
+  };
+
+  return (
+    <div>
+      <TextBox value={text} onChange={setText} />
+      <Buttons onLog={handleLog} onClear={handleClear} />
+    </div>
+  );
+};
+
+export default Home;
